60c77e7e3050b33c91355ad727db5a917c8899e9
Nirmal-P <p.nirmalpatel1996@gmail.com>
2021-01-16 18:01:17 +0530

    try to make boat move to point

--- Squash the Creep/squash_the_creeps_start/Player.gd
+++ Squash the Creep/squash_the_creeps_start/Player.gd
@@ -1,69 +1,26 @@
 extends KinematicBody
-
-const ARRIVE_THRESHOLD: = 100.0
-# speed in m/s
-export var max_speed = 500
-#export var target_path: = NodePath()
-#Down acceleration in m/s^2
-export var fall_acceleration = 75
-
-var _velocity = Vector2.ZERO
-var _velocity3 = Vector3.ZERO
-
-func _physics_process(_delta: float) -> void:
-	var target_global_position: = Vector2.ZERO
-	var direction: = Vector3.ZERO
-	var global_pos = vec2_vec3($Pivot.get_translation(),2)
-	if Input.is_mouse_button_pressed(1):
-		target_global_position = get_viewport().get_mouse_position()
-	else:
-		return
-	_velocity3 = vec3_vec2(Steering.follow(
-		_velocity,
-		global_pos,
-		target_global_position,
-		max_speed
-	), 2, 0)
+var pointlist = [[50,0,10], [5,0,30] ,[20,0,0]]
+var speed = 20
+var number_of_points = len(pointlist)
+var i = 0
+var a = null
+var b = null
+func _process(delta):
 	
-	direction = _velocity3.normalized()
-	$Pivot.look_at(translation + direction, Vector3.UP)
-	_velocity3 = move_and_slide(_velocity3, Vector3.UP)
-#
+	for p in pointlist:
+		a = p[0]
+		b = p[2]
+		var point =Vector3(a, 0, b) 
+		
+		var direction
+		if point.distance_to(transform.origin) > 0.05:
+			direction = point - transform.origin
+			direction = direction.normalized() * speed
 
-func vec3_vec2(vec2, axis, value):
-	var array = [vec2.x, vec2.y]
-	array.insert(axis, value)
-	return Vector3(array[0], array[1], array[2])
+		else:
+			direction = point - transform.origin
 
-func vec2_vec3(vec3, axis):
-	var array = [vec3.x, vec3.y, vec3.z]
-	array.remove(axis)
-	return Vector2(array[0], array[1])
+		move_and_slide(direction)
 
-#func _physics_process(delta):
-#	#local variable to store input direction
-#	var direction = Vector3.ZERO
-#
-#	#check the input and update the direction accordingly
-#	if Input.is_action_pressed("move_right"):
-#		direction.x += 1
-#	if Input.is_action_pressed("move_left"):
-#		direction.x -= 1
-#	if Input.is_action_pressed("move_back"):
-#		direction.z += 1
-#	if Input.is_action_pressed("move_forward"):
-#		direction.z -= 1
-#
-#	#...when 2 keys are pressed at a time
-#	if direction != Vector3.ZERO:
-#		direction = direction.normalized()
-#		$Pivot.look_at(translation + direction, Vector3.UP)
-#
-#	#Ground velocity
-#	velocity.x = direction.x * speed
-#	velocity.z = direction.z * speed
-#	#Vertical velocity
-#	velocity.y -= fall_acceleration * delta
-#	# Moving the character
-#	velocity = move_and_slide(velocity, Vector3.UP)
-	
+
+
