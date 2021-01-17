ed7c081b74fdb3755c8789fadd81422d420065e7
colorizer <64630824+colorizer@users.noreply.github.com>
2021-01-17 16:13:39 +0530

    A working mousetrack

--- Squash the Creep/squash_the_creeps_start/Main.gd
+++ Squash the Creep/squash_the_creeps_start/Main.gd
@@ -15,12 +15,9 @@ func _ready():
 
 func _physics_process(delta):
 	mouse_path()
-	if Input.is_action_just_released("Click"):
+	if !Input.is_action_pressed("Click"):
 		followmouse(delta)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func mouse_path():
 	var ray_length = 1000
@@ -29,12 +26,15 @@ func mouse_path():
 	var from = camera.project_ray_origin(mouse_pos)
 	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
 	var cursor_pos = dropPlane.intersects_ray(from,to)
+#	cursor_pos.y = 1.0
+#	if Input.is_action_just_pressed("Click"):
+#		mousepath.global_transform.origin = cursor_pos
 	if Input.is_action_pressed("Click"):
 		mousepath.curve.add_point(cursor_pos)
 		print(mousepath.curve.get_point_count())
 	cursor.global_transform.origin = cursor_pos
 
 func followmouse(delta):
-	mousefollow.set_offset(mousefollow.get_offset() + 5*delta)
+	mousefollow.set_offset(mousefollow.get_offset() + 10*delta)
 	print(mousefollow.get_offset())
 	pass
