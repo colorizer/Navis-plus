e98b60bc2f315f904f3c16893885f753409ea9e5
Nirmal-P <p.nirmalpatel1996@gmail.com>
2021-01-18 00:17:30 +0530

    people_implementation

---
+++ Squash the Creep/squash_the_creeps_start/boy.gd
@@ -0,0 +1,18 @@
+extends Area
+
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	rotate_y(deg2rad(0.025))  #remove this line to remove rotation
+	
+
+
+func _on_boy_body_entered(body):
+	if body.name == "Cursor2follow": #we may change this cursor2follow name
+		$Timer.start()
+
+
+func _on_Timer_timeout():
+	queue_free()
