d2fb7b1a086e6030839cfe5fe6b78ed466e61348
colorizer <64630824+colorizer@users.noreply.github.com>
2021-01-26 23:39:54 +0530

    levels

--- Dev files/level/Level1.gd
+++ Dev files/level/Level1.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var max_time = 20.0
+var max_time = 10.0
 
 func _ready():
 	pass
--- Dev files/level/Level2.gd
+++ Dev files/level/Level2.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var max_time = 15.0
+var max_time = 20.0
 
 func _ready():
 	pass
